593aeace4a506eb441ef27b5503043e548b3adca
Anmol Thakur <at2314541@gmail.com>
2025-04-16 15:09:16 +0530

    n

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ const express = require("express");
 const app = express();
 
 const db = require("./config/db");
-app.use(express.urlencoded({ extended: true, limit: "50mb" }));
-
+app.use(express.urlencoded({ extended: false, limit: "50mb" }));
 app.use(express.json());
+const cors=require('cors')
+app.use(cors());
 
 const adminroutes = require("./routes/apiroutes");
 app.use("/api", adminroutes);
@@ -16,7 +17,7 @@ app.use("/", (req, res) => {
   res.send("Welcome");
 });
 
-app.listen(3000, (err) => {
-  if (err) throw console.log("Server error occur!");
-  else console.log("Server running at port no. " + 3000);
+app.listen(4000, (err) => {
+  if (err) console.log("Server error occur!");
+  else console.log("Server running at port no. " + 4000);
 });
